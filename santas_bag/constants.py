a759ee77fa65f2109113accba14682b6f76aeba8
loew-tech <147945762+loew-tech@users.noreply.github.com>
2026-06-10 16:24:26 -0600

    Constants (#2)
    * constants
    
    * fixing ALHPAS_TO_NUMS to include uppercase ascii

--- santas_bag/constants.py
+++ santas_bag/constants.py
@@ -1,3 +1,6 @@
+from operator import add, mul, sub, truediv, floordiv, mod
+from string import ascii_lowercase, ascii_uppercase
+
 CARDINAL_DIRECTIONS = (
     (1, 0),
     (0, 1),
@@ -16,3 +19,15 @@ ALL_DIRECTIONS = (
     *CARDINAL_DIRECTIONS,
     *DIAGONAL_DIRECTIONS,
 )
+
+NUMS_TO_ALPHAS_LOWERCASE = {i: v for i, v in enumerate(ascii_lowercase)}
+NUMS_TO_ALPHAS_UPPERCASE = {i: v for i, v in enumerate(ascii_uppercase)}
+ALPHAS_TO_NUMS = {
+    *{v: i for i, v in enumerate(ascii_lowercase)},
+    *{v: i for i, v in enumerate(ascii_uppercase)}
+}
+
+OPS_DICT = {'+': add, '*': mul, '-': sub, '/': truediv, '//': floordiv, '%': mod, '**': pow}
+
+REGEX_WORDS = r'\b[a-zA-Z]+\b'
+REGEX_DIGITS = r'-?\d+'
